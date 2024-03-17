7773e3da200cc30d3f33533fecbe78b5f4208865
Rimsha Qureshi <160319312+rimshaqureshii@users.noreply.github.com>
2024-03-17 14:53:07 -0700

    Add files via upload

---
+++ personal_message.js
@@ -0,0 +1,3 @@
+// //Question
+var person = 'Eric';
+console.log("Hello ".concat(person, ", would you like to learn some python today"));
---
+++ personal_message.ts
@@ -0,0 +1,11 @@
+ // //Question 1
+ //Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+let hello: string = "Hello Eric"
+  console.log(`${hello}, Would you like to learn some python Today?`);
+  
+    
+
+
+
+
+
